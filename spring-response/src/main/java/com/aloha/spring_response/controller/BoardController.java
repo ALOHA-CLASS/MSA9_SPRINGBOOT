dee1ee92cc8cddee47a8285fef9871ba32d12e64
wwwalohacampus <h850415@naver.com>
2024-11-12 12:31:46 +0900

    💌response 완료

--- spring-response/src/main/java/com/aloha/spring_response/controller/BoardController.java
+++ spring-response/src/main/java/com/aloha/spring_response/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.aloha.spring_response.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -44,48 +47,100 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/board")
 public class BoardController {
 
+    /**
+     * 게시글 목록
+     * 🔗 /board/list
+     * 🎁 model : boardList
+     * @param model
+     * @return
+     */
     @GetMapping("/list")
     public String list(Model model) {
+        log.info("[GET] - /board/list");
 
+        // 데이터 요청
         List<Board> boardList = new ArrayList<>();
-        boardList.add(Board.builder().title("제목1").writer("작성자1").content("내용1").build());
-        boardList.add(Board.builder().title("제목2").writer("작성자2").content("내용2").build());
-        boardList.add(Board.builder().title("제목3").writer("작성자3").content("내용3").build());
+        boardList.add(new Board(1, "제목1","작성자1","내용1"));
+        boardList.add(new Board(2, "제목2","작성자2","내용2"));
+        boardList.add(new Board(3, "제목3","작성자3","내용3"));
 
+        // 모델 등록
         model.addAttribute("boardList", boardList);
+
+        // 뷰 페이지 지정
         return "board/list";
     }
 
+   
     /**
-     * /board/update?no=10
+     * 게시글 조회
+     * 🔗 /board/read?no=⭐
+     * 🎁 model : board
+     * @param model
+     * @param no
      * @return
      */
     @GetMapping("/read")
     public String read(Model model, @RequestParam("no") int no) {
+        log.info("[GET] - /board/read");
         log.info("no : " + no);
-        // 데이터 조회 요청
+
+        // 데이터 요청
         Board board = Board.builder()
                             .no(no)
                             .title("제목")
                             .writer("작성자")
                             .content("내용")
                             .build();
+        // 모델 등록
         model.addAttribute("board", board);
+
+        // 뷰 페이지 지정
         return "board/read";
     }
 
+    /**
+     * 게시글 등록
+     * [GET]
+     * 🔗 /board/insert
+     * @return
+     */
     @GetMapping("/insert")
     public String insert() {
+        // 뷰 페이지 지정
         return "board/insert";
     }
 
+    /**
+     * 게시글 등록 처리
+     * [POST]
+     * 🔗 /board/insert
+     * 💌 <form> ➡ Board
+     * @param board
+     * @return
+     */
     @PostMapping("/insert")
     public String insertPost(Board board) {
+        log.info("[POST] - /board/insert");
         log.info("board : " + board);
+
+        // 데이터 등록 요청
+        int result = new Random().nextInt(2);       // 0 또는 1
+        if( result ==  0 )
+            return "redirect:/board/insert";    // redirect ➡ 글등록 페이지
         
-        return "redirect:/board/list";
+        return "redirect:/board/list";          // redirect ➡ 글목록 페이지
     }
 
+    /**
+     * 게시글 수정
+     * [GET]
+     * 🔗 /board/update
+     * 🎁 model : board
+     * @param model
+     * @param no
+     * @return
+     */
     @GetMapping("/update")
     public String update(Model model, @RequestParam("no") int no) {
         log.info("no : " + no);
@@ -96,35 +151,94 @@ public class BoardController {
                             .writer("작성자")
                             .content("내용")
                             .build();
+        // 모델 등록
         model.addAttribute("board", board);
+
+        // 뷰 페이지 지정
         return "board/update";
     }
 
+    /**
+     * 게시글 수정 처리
+     * [POST]
+     * 🔗 /board/update
+     * 💌 <form> ➡ board
+     * @param board
+     * @return
+     */
     @PostMapping("/update")
     public String updatePost(Board board) {
         log.info("board : " + board);
+
+        // 데이터 수정 요청
+        int result = new Random().nextInt(2);
+        if( result == 0 )
+            // redirect ➡ /board/update?no=⭐
+            return "redirect:/board/update?no=" + board.getNo();    
         
+        // redirect ➡ /board/list
         return "redirect:/board/list";
     }
 
+    /**
+     * 게시글 삭제 처리
+     * [POST]
+     * 🔗 /board/delete
+     * 💌 <form> ➡ no
+     * @param no
+     * @return
+     */
     @PostMapping("/delete")
     public String deletePost(@RequestParam("no") int no) {
         log.info("no : " + no);
-        
+        // 데이터 삭제 요청
+        int result = new Random().nextInt(2);
+        if( result == 0 )
+            // redirect ➡ /board/update?no=⭐
+            return "redirect:/board/update?no=" + no;    
+
         return "redirect:/board/list";
     }
     
+    /**
+     * 게시글 수정 비동기 처리
+     * [PUT]
+     * 🔗 /board
+     * 💌 JSON ➡ Board   (⭐@RequestBody)
+     * @param board
+     * @return
+     */
     @ResponseBody
     @PutMapping("")
     public ResponseEntity<String> putUpdate(@RequestBody Board board) {
-        
+        log.info("[PUT] - /board");
+        log.info("board : " + board);
+
+        int result = new Random().nextInt(2);
+        if( result  == 0 )
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body("FAIL");
         
         return ResponseEntity.ok("SUCCESS");
     }
 
+    /**
+     * 게시글 삭제 비동기 처리
+     * [DELETE]
+     * 🔗 /board?no=⭐
+     * @param no
+     * @return
+     */
     @ResponseBody
     @DeleteMapping
     public ResponseEntity<String> delete(@RequestParam("no") int no) {
+        log.info("[DELETE] - /board");
+        log.info("no : " + no);
+
+        int result = new Random().nextInt(2);
+        if( result == 0 ) 
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body("FAIL");
 
         return ResponseEntity.ok("SUCCESS");
     }
